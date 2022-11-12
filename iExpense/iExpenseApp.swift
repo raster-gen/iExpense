323867327fe57149aa35c9fba07ce53d18866c3f
raster-gen <rasterroot@gmail.com>
2022-11-12 11:26:31 +0000

    init commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gennady Raster on 12.11.22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
