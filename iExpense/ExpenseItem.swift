0cb1963b697af54df7e2c373802503183565af03
raster-gen <rasterroot@gmail.com>
2022-11-13 11:01:07 +0000

    created ExpenseItem file with struct

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Gennady Raster on 13.11.22.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    var name: String
+    var type: String
+    var amount: Double
+}
